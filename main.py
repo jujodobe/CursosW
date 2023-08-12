5a42ab329957d25f81008b1d16d63dd72a3dfd4b
juan.dossantos <jds.infosystem@gmail.com>
2023-08-12 19:26:45 -0400

    cambios

---
+++ main.py
@@ -0,0 +1,5 @@
+from vehiculo import Vehiculo
+
+var = Vehiculo('2019', {'Motor': 1.3, 'TipoRueda': '14-65-32'})
+print(var.getChapa())
+
---
+++ vehiculo.py
@@ -0,0 +1,11 @@
+class Vehiculo:
+
+    def __init__(self, modelo, chapa):
+        self.modelo = modelo
+        self.chapa = chapa
+
+
+    def calcularEdad(self):
+        return self.chapa
+
+
